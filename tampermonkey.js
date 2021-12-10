b9fa4d8524f375f161c7470f6587f18771e2ba1c
ProB1 <50064486+iProB1@users.noreply.github.com>
2021-12-10 23:48:57 +0300

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Discord Developer Mode
+// @description  https://github.com/iProB1/Discord-Developer
+// @namespace    ProB1#1337
+// @version      1.0
+// @author       You
+// @match        https://discord.com/*
+// @homepageURL  https://github.com/iProB1/Discord-Developer
+// @grant        none
+// @license       MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+   Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true});
+})();
